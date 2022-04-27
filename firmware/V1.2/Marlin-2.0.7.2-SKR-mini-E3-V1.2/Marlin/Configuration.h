4ee3472b0f23dfc1bddc1a70cd1f4c7c553b9588
Torben Lottmann <torben@torbstoff.de>
2022-04-27 22:41:00 +0200

    update offsets and extruder steps

--- firmware/V1.2/Marlin-2.0.7.2-SKR-mini-E3-V1.2/Marlin/Configuration.h
+++ firmware/V1.2/Marlin-2.0.7.2-SKR-mini-E3-V1.2/Marlin/Configuration.h
@@ -736,7 +736,7 @@
  * Override with M92
  *                                      X, Y, Z, E0 [, E1[, E2...]]
  */
-#define DEFAULT_AXIS_STEPS_PER_UNIT   { 80, 80, 400, 130 }
+#define DEFAULT_AXIS_STEPS_PER_UNIT   { 80, 80, 400, 137 }
 
 /**
  * Default Max Feed Rate (mm/s)
@@ -981,7 +981,7 @@
  *     |    [-]    |
  *     O-- FRONT --+
  */
-#define NOZZLE_TO_PROBE_OFFSET { -42, -5, -1.665 }
+#define NOZZLE_TO_PROBE_OFFSET { -41, -10, -1.76 }
 
 // Most probes should stay away from the edges of the bed, but
 // with NOZZLE_AS_PROBE this can be negative for a wider probing area.
